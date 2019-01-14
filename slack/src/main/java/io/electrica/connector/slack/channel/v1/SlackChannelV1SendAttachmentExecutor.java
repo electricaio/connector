450d019f324a334340372869c759c8f0ded68ea7
MunishSodhi <ermunishsodhi@gmail.com>
2019-01-14 18:54:06 +0530

    MAIN-266 - Add support for attachments in Slack Connector
    -- Changes as per review comments

--- slack/src/main/java/io/electrica/connector/slack/channel/v1/SlackChannelV1SendAttachmentExecutor.java
+++ slack/src/main/java/io/electrica/connector/slack/channel/v1/SlackChannelV1SendAttachmentExecutor.java
@@ -1,6 +1,11 @@
 package io.electrica.connector.slack.channel.v1;
 
-import io.electrica.connector.slack.channel.v1.model.SlackChannelV1SendTextPayload;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.electrica.connector.slack.channel.v1.model.Attachment;
+import io.electrica.connector.slack.channel.v1.model.SlackChannelV1SendAttachmentPayload;
 import io.electrica.connector.spi.ConnectorExecutor;
 import io.electrica.connector.spi.ServiceFacade;
 import io.electrica.connector.spi.exception.Exceptions;
@@ -9,31 +14,39 @@ import okhttp3.*;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
+import java.util.List;
 
 import static io.electrica.connector.spi.Validations.requiredPayloadField;
 
 public class SlackChannelV1SendAttachmentExecutor implements ConnectorExecutor {
 
+    private static final Integer MAX_ATTACHMENTS = 20;
     private final OkHttpClient httpClient;
-
     private final String url;
-    private final SlackChannelV1SendTextPayload payload;
+    private final SlackChannelV1SendAttachmentPayload payload;
+    private final ObjectMapper mapper;
 
     SlackChannelV1SendAttachmentExecutor(
             OkHttpClient httpClient,
             String urlTemplate,
-            ServiceFacade facade
+            ServiceFacade facade,
+            ObjectMapper mapper
     ) throws IntegrationException {
         this.httpClient = httpClient;
         url = String.format(urlTemplate, facade.getTokenAuthorization().getToken());
-        payload = facade.readPayload(SlackChannelV1SendTextPayload.class);
+        payload = facade.readPayload(SlackChannelV1SendAttachmentPayload.class);
+        this.mapper = mapper;
     }
 
     @Nullable
     @Override
     public Object run() throws IntegrationException {
-        String message = requiredPayloadField(payload.getMessage(), "message");
-        RequestBody body = RequestBody.create(MediaType.get("application/json"), message);
+        List<Attachment> attachments = requiredPayloadField(payload.getAttachments(), "attachments");
+        if (attachments.size() > MAX_ATTACHMENTS) {
+            throw Exceptions.validation("Restrict the attachments to less than " + MAX_ATTACHMENTS);
+        }
+        String toJson = createAttachmentMessage(attachments);
+        RequestBody body = RequestBody.create(MediaType.get("application/json"), toJson);
 
         Request request = new Request.Builder()
                 .url(url)
@@ -49,5 +62,15 @@ public class SlackChannelV1SendAttachmentExecutor implements ConnectorExecutor {
         } catch (IOException e) {
             throw Exceptions.io("Network error", e);
         }
+    }
+
+    private String createAttachmentMessage(List<Attachment> attachments) {
+        ObjectNode parent = mapper.createObjectNode();
+        ArrayNode arrayNode = parent.putArray("attachments");
+        for (Attachment attachment : attachments) {
+            JsonNode node = mapper.valueToTree(attachment);
+            arrayNode.add(node);
+        }
+        return parent.toString();
     }
 }
--- slack/src/test/java/io/electrica/connector/slack/channel/v1/SlackChannelV1SendAttchmentExecutorFactoryTest.java
+++ slack/src/test/java/io/electrica/connector/slack/channel/v1/SlackChannelV1SendAttchmentExecutorFactoryTest.java
@@ -1,7 +1,9 @@
 package io.electrica.connector.slack.channel.v1;
 
+import io.electrica.connector.slack.channel.v1.model.Attachment;
+import io.electrica.connector.slack.channel.v1.model.Field;
 import io.electrica.connector.slack.channel.v1.model.SlackChannelV1Action;
-import io.electrica.connector.slack.channel.v1.model.SlackChannelV1SendTextPayload;
+import io.electrica.connector.slack.channel.v1.model.SlackChannelV1SendAttachmentPayload;
 import io.electrica.connector.spi.ServiceFacade;
 import io.electrica.connector.spi.Validations;
 import io.electrica.connector.spi.exception.ExceptionCodes;
@@ -12,9 +14,16 @@ import io.electrica.connector.test.MapConnectorProperties;
 import io.electrica.connector.test.security.SecuredAuthorizations;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
 class SlackChannelV1SendAttchmentExecutorFactoryTest {
 
     private static final String TEST_CHANNEL_TOKEN = "" +
@@ -23,9 +32,12 @@ class SlackChannelV1SendAttchmentExecutorFactoryTest {
             "vL0TAVED5SFNkHq8InEPUUaxcFxbzOpx9O+yU3cJjyEotrfm+E71IJbSJdoGKQGrMouDfki2c4a59/YM0JA3gYKvzTiN5pyFXSuS1OS0" +
             "Bp7AGE8KlMv1/oE0k/PeGCByN4soVA==";
 
-    private static final String ATTACHMENT_MESSAGE = "{\"attachments\":[{\"color\":\"#36a64f\",\"pretext\":" +
-            "\"Slack Attachment Test\",\"title\":\"Test Title\",\"text\":\"Optional text that appears within the " +
-            "attachment\",\"fields\":[]}]}";
+    private List<Attachment> attachments;
+
+    @BeforeAll
+    void setup() {
+        attachments = createAttachmentList();
+    }
 
     private static ElectricaEmulator emulator;
 
@@ -45,24 +57,24 @@ class SlackChannelV1SendAttchmentExecutorFactoryTest {
     void testUnsupportedActionThrowException() {
         InvocationContext context = InvocationContext.builder("unsupported-action")
                 .authorization(SecuredAuthorizations.token(TEST_CHANNEL_TOKEN))
-                .payload(new SlackChannelV1SendTextPayload().message(ATTACHMENT_MESSAGE))
+                .payload(new SlackChannelV1SendAttachmentPayload().attachments(attachments))
                 .build();
         IntegrationException e = assertThrows(IntegrationException.class, () -> emulator.runIntegration(context));
         assertEquals(ExceptionCodes.VALIDATION, e.getCode());
     }
 
     @Test
-    void testSendText() throws IntegrationException {
+    void testSendAttachmentMessage() throws IntegrationException {
         InvocationContext context = InvocationContext.builder(SlackChannelV1Action.SENDATTACHMENT)
                 .authorization(SecuredAuthorizations.token(TEST_CHANNEL_TOKEN))
-                .payload(new SlackChannelV1SendTextPayload().message(ATTACHMENT_MESSAGE))
+                .payload(new SlackChannelV1SendAttachmentPayload().attachments(attachments))
                 .build();
         Object result = emulator.runIntegration(context);
         assertNull(result);
     }
 
     @Test
-    void testSendTextMissedPayloadThrowException() {
+    void testSendAttachmentMissedPayloadThrowException() {
         InvocationContext context = InvocationContext.builder(SlackChannelV1Action.SENDATTACHMENT)
                 .authorization(SecuredAuthorizations.token(TEST_CHANNEL_TOKEN))
                 .build();
@@ -73,25 +85,65 @@ class SlackChannelV1SendAttchmentExecutorFactoryTest {
     }
 
     @Test
-    void testSendTextMissedPayloadMessageThrowException() {
+    void testSendAttachmentMissedPayloadMessageThrowException() {
         InvocationContext context = InvocationContext.builder(SlackChannelV1Action.SENDATTACHMENT)
                 .authorization(SecuredAuthorizations.token(TEST_CHANNEL_TOKEN))
-                .payload(new SlackChannelV1SendTextPayload())
+                .payload(new SlackChannelV1SendAttachmentPayload())
                 .build();
 
         IntegrationException e = assertThrows(IntegrationException.class, () -> emulator.runIntegration(context));
         assertEquals(ExceptionCodes.VALIDATION, e.getCode());
-        assertEquals(Validations.requiredPayloadFieldErrorMessage("message"), e.getMessage());
+        assertEquals(Validations.requiredPayloadFieldErrorMessage("attachments"), e.getMessage());
     }
 
     @Test
-    void testSendTextMissedAuthorizationThrowException() {
+    void testSendAttachmentMissedAuthorizationThrowException() {
         InvocationContext context = InvocationContext.builder(SlackChannelV1Action.SENDATTACHMENT)
-                .payload(new SlackChannelV1SendTextPayload())
+                .payload(new SlackChannelV1SendAttachmentPayload())
                 .build();
 
         IntegrationException e = assertThrows(IntegrationException.class, () -> emulator.runIntegration(context));
         assertEquals(ExceptionCodes.VALIDATION, e.getCode());
         assertEquals(ServiceFacade.REQUIRED_TOKEN_AUTHORIZATION_ERROR_MESSAGE, e.getMessage());
+    }
+
+    @Test
+    void testSendMoreThan20AttachmentsInMessageThrowsException() throws IntegrationException {
+        List<Attachment> attachmentsList = new ArrayList<>();
+        for (int i = 0; i < 21; i++) {
+            attachmentsList.add(createAttachment());
+        }
+        InvocationContext context = InvocationContext.builder(SlackChannelV1Action.SENDATTACHMENT)
+                .authorization(SecuredAuthorizations.token(TEST_CHANNEL_TOKEN))
+                .payload(new SlackChannelV1SendAttachmentPayload().attachments(attachmentsList))
+                .build();
+        IntegrationException e = assertThrows(IntegrationException.class, () -> emulator.runIntegration(context));
+        assertEquals(ExceptionCodes.VALIDATION, e.getCode());
+    }
+
+    private List<Attachment> createAttachmentList() {
+        Attachment attachment = createAttachment();
+        List<Attachment> attachmentList = new ArrayList<>();
+        attachmentList.add(attachment);
+        return attachmentList;
+    }
+
+    private Attachment createAttachment() {
+        Attachment attachment = new Attachment();
+        attachment.setColor("#36a64f");
+        attachment.setPretext("Slack Attachment Test");
+        attachment.setAuthorName("Electrica");
+        attachment.setAuthorLink("https://www.electrica.io/");
+        attachment.setTitle("Test Title");
+        attachment.setTitleLink("https://www.electrica.io/");
+        attachment.setText("Optional text that appears within the attachment");
+        Field field = new Field();
+        field.setShort(false);
+        field.setTitle("Priority");
+        field.setValue("High");
+        attachment.setFields(Collections.singletonList(field));
+        attachment.setFooter("Slack Message Footer");
+        attachment.setTs(new Date().getTime() / 1000);
+        return attachment;
     }
 }
