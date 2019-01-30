d356742d99e94e78a2ca2499004c41eec6d73f7a
Chris McCaw <developer.cmccaw@gmail.com>
2019-01-30 13:01:46 -0800

    Updated hackerrank tests with new access key

---
+++ hackerrank/src/test/java/io/electrica/connector/hackerrank/v3/TestConstants.java
@@ -0,0 +1,11 @@
+package io.electrica.connector.hackerrank.v3;
+
+public interface TestConstants {
+
+    String HR_ACCESS_TOKEN = "oiiHBeXP4cEU4qScT5vlJxGQ2BIFXyXJ2/wVrPghtHX4IKBtoSyENbwi8KLwWrsO" +
+            "CgOuq3etb68HNaeSyeZ4EAfJ9Qo3GDwMC1yPSJd/F6e0tHnis3oIFgvuSyI0XO3R38HPEiBhacFdftJjtkqwLQbKYLXuYUw/xO8EfBr" +
+            "rRt3AV6bPrujKeVzj++10nDCk/JNYkJPbC+8KpaDb/6iO7ZoA2qmP8JfZvp9jipD3lUwnM54zUC0s4euAtlBpnPqCs5laa8A8DrvrK7" +
+            "ntjWEX3kg8Ok01HURIfSlf7RqqDcdKX7E7H34PwnPHyar6o9Ly3NnMnDzqlAQFlcQ73QeLoA==";
+
+    int TEST_ID = 367946;
+}
